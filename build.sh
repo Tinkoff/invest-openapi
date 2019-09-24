36cb0596be2d72915c33088e77ab55e46453f2c2
Nikita Melnikov <n.v.melnikov@tinkoff.ru>
2019-09-24 16:45:22 +0300

    Add swwagger in /docs

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+rm -rf docs
+
 docker run -v $PWD:/docs squidfunk/mkdocs-material build --config-file=/docs/src/mkdocs.yml --site-dir ../docs
+
+mv src/swagger-ui docs/
+mv src/swagger.yaml docs/swagger-ui/
