76294ccc8425279476c88067a3253a050248cd5a
Nikita Melnikov <n.v.melnikov@tinkoff.ru>
2019-10-09 11:47:23 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ocker run --rm -it -v ~/.ssh:/root/.ssh -v $PWD:/docs squidfunk/mkdocs-material gh-deploy --config-file=/docs/src/mkdocs.yml
+docker run --rm -it -v ~/.ssh:/root/.ssh -v $PWD:/docs squidfunk/mkdocs-material gh-deploy --config-file=/docs/src/mkdocs.yml
